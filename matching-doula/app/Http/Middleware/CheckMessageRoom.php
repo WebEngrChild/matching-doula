641579f4bb5e65b9e0f6b8a1f34af0ff917f6a13
joe-main <qmmplusfact@gmail.com>
2021-08-04 17:44:13 +0900

    部外者からの入室&メッセージ送信防止用のmiddleware実装

---
+++ matching-doula/app/Http/Middleware/CheckMessageRoom.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+//リアルタイムチャット機能認証
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\MessageRoom;
+use Illuminate\Http\Request;
+
+use Closure;
+
+class CheckMessageRoom
+{
+    const MESSAGEROOM = 'messagesroom';
+    const SLASHSIGN = '/';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $messageroomid = (int) str_replace(self::MESSAGEROOM, "",strstr(str_replace(self::SLASHSIGN, "", $request->getRequestUri()),self::MESSAGEROOM));
+
+        if(Auth::check()){
+            $user = Auth::user();
+            $meesagerooms = $user->messageRooms()->get();
+            $check = false;
+
+            if($meesagerooms->isEmpty()) {
+                return redirect()->route('top');
+            };
+
+            foreach ($meesagerooms as $meesageroom) {
+            if($meesageroom->id === $messageroomid ) {
+                $check = true;
+                break;
+            };
+          };
+
+            if($check<>true) {
+              return redirect()->route('top');
+          };
+        };
+         return $next($request);
+    }
+}
