c94ee477fa45b2c60780907584eac62771570bd8
joe-main <qmmplusfact@gmail.com>
2021-08-12 18:28:43 +0900

    修正

---
+++ matching-doula/tests/Feature/ItemControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Tests\Feature;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class ItemControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    //商品一覧
+    public function testshowItems() {
+            $response = $this->get(route('top'));
+            $response->assertStatus(200)
+                ->assertViewIs('items.items');
+    }
+    //未ログイン出品
+    public function testGuestSell() {
+            $response = $this->get(route('sell'));
+            $response->assertRedirect(route('login'));
+    }
+
+    //ログイン済み出品
+    public function testAuthSell() {
+            $user = factory(User::class)->create();
+            $response = $this->actingAs($user)
+                ->get(route('sell'));
+            $response->assertStatus(302);
+    }
+}
