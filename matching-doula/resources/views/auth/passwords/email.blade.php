19847bc4acffc30cc5520ebb9c83ee1c93050f71
joe-main <qmmplusfact@gmail.com>
2021-08-18 18:50:45 +0900

    favicon,home画像修正

--- matching-doula/resources/views/auth/passwords/email.blade.php
+++ matching-doula/resources/views/auth/passwords/email.blade.php
@@ -31,7 +31,7 @@
                     </div>
 
                     <div class="form-group mb-0">
-                        <button type="submit" class="btn btn-block btn-secondary">
+                        <button type="submit" class="btn btn-secondary text-white btn-block">
                             送信する
                         </button>
                     </div>
