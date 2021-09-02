22212e4fc027b165b091fdeb9a3c94327478c8fb
joe-main <qmmplusfact@gmail.com>
2021-09-02 15:35:04 +0900

    Pay.jpを用いた処理のコメント追加

--- matching-doula/resources/views/items/item_buy_form.blade.php
+++ matching-doula/resources/views/items/item_buy_form.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-{{-- PAY.JPのライブラリを読み込む --}}
+{{-- PAY.JPのクライアントサイド向けのライブラリを読み込む --}}
 <script src="https://js.pay.jp/v2/pay.js"></script>
 <div class="container">
     <div class="row">
@@ -24,7 +24,7 @@
                 'item' => $item
             ])
 
-        {{-- クレジットカード情報を入力するフォームはPAY.JPのライブラリを組み込むことで初めて動作するようになります。 --}}
+        {{-- クレジットカード情報を入力するフォームはPAY.JPのライブラリを組み込むことで動作 --}}
             <div class="row">
                 <div class="col-8 offset-2">
                     <div class="card-form-alert alert alert-danger" role="alert" style="display: none"></div>
@@ -46,13 +46,14 @@
 
             <div class="row mt-3 mb-3">
                 <div class="col-8 offset-2">
-                    {{-- カードトークン取得 --}}
+                    {{-- 購入ボタンを押下でカード情報をPAY.JPサーバに送信してカードトークン取得 --}}
                     <button class="btn btn-secondary text-white btn-block" onclick="onSubmit(event)">購入</button>
                 </div>
             </div>
 
             <form id="buy-form" method="POST" action="{{route('item.buy', [$item->id])}}">
                 @csrf
+                {{-- formタグの中に、あらかじめ以下のinputタグを用意 --}}
                 <input type="hidden" id="card-token" name="card-token">
             </form>
         </div>
@@ -60,14 +61,18 @@
 </div>
 
 <script>
-    // カード情報入力フォームの構築
+    // Payjpクラスのインスタンス取得
     var payjp = Payjp('{{config("payjp.public_key")}}')
 
+    //カード情報入力フォームを構築するためのインスタンス
     var elements = payjp.elements()
 
+    //createメソッドで、指定した種類のElementインスタンスを生成
     var numberElement = elements.create('cardNumber')
     var expiryElement = elements.create('cardExpiry')
     var cvcElement = elements.create('cardCvc')
+
+    //mountメソッドで各種入力欄をDOM上に配置
     numberElement.mount('#number-form')
     expiryElement.mount('#expiry-form')
     cvcElement.mount('#cvc-form')
@@ -77,6 +82,8 @@
     function onSubmit(event) {
           const msgDom = document.querySelector('.card-form-alert');
           msgDom.style.display = "none";
+
+          //PAY.JPサーバにカード情報を送信し、カードトークンを取得
           payjp.createToken(numberElement).then(function(r) {
 
               if (r.error) {
@@ -85,7 +92,10 @@
                   return;
               }
 
+              //CSSセレクタでidがcard-tokenのDOMを検索し、value属性にカードトークンを設定
               document.querySelector('#card-token').value = r.id;
+
+              //フォーム送信
               document.querySelector('#buy-form').submit();
           })
       }
